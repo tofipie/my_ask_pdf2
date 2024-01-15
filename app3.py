4ba23a8a571e1236bd86d5ab5c992064b2e49395
tofipie <62646269+tofipie@users.noreply.github.com>
2024-01-15 13:30:59 +0200

    Update app3.py

--- app3.py
+++ app3.py
@@ -39,16 +39,20 @@ def create_RetrievalQA_chain(query):
   answer = qa({"query": query})['result']
   return answer
 
- prompt_template = """Human: Use the following pieces of context to provide a concise answer to the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
-                      <context>
-                      {context}
-                      </context
+prompt_template = """
 
-                     Question: {question}
+Human: Use the following pieces of context to provide a concise answer to the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
+<context>
+{context}
+</context
 
-                     Assistant:"""
+Question: {question}
 
-PROMPT = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+Assistant:"""
+
+PROMPT = PromptTemplate(
+    template=prompt_template, input_variables=["context", "question"]
+)
 
 embeddings = HuggingFaceEmbeddings(
         model_name="sentence-transformers/all-MiniLM-L6-v2",
