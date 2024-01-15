2b9efb6354d839260aae9ce78f064b7396ce0bc9
tofipie <62646269+tofipie@users.noreply.github.com>
2024-01-15 13:30:06 +0200

    Update app3.py

--- app3.py
+++ app3.py
@@ -38,7 +38,18 @@ def create_RetrievalQA_chain(query):
   
   answer = qa({"query": query})['result']
   return answer
-        
+
+ prompt_template = """Human: Use the following pieces of context to provide a concise answer to the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
+                      <context>
+                      {context}
+                      </context
+
+                     Question: {question}
+
+                     Assistant:"""
+
+PROMPT = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+
 embeddings = HuggingFaceEmbeddings(
         model_name="sentence-transformers/all-MiniLM-L6-v2",
         model_kwargs={"device": "cpu"},
@@ -78,18 +89,7 @@ def main():
         ''')
         st.write('Made by Noa Cohen')
             
-    prompt_template = """Human: Use the following pieces of context to provide a concise answer to the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
-                      <context>
-                      {context}
-                      </context
-
-                     Question: {question}
-
-                     Assistant:"""
-
-    PROMPT = PromptTemplate(
-            template=prompt_template, input_variables=["context", "question"])
-
+  
 
 
     user_input = st.text_area("Ask Your Question")
