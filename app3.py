2939ad9333ec3afea7aa12b1af899848c4d27319
tofipie <62646269+tofipie@users.noreply.github.com>
2024-01-15 13:27:08 +0200

    Update app3.py

--- app3.py
+++ app3.py
@@ -27,7 +27,18 @@ from langchain.text_splitter import CharacterTextSplitter, RecursiveCharacterTex
 from langchain.chains.question_answering import load_qa_chain
 
 
+def create_RetrievalQA_chain(query):
 
+  qa = RetrievalQA.from_chain_type(
+    llm=llm,
+    chain_type="stuff",
+    retriever=vectorstore_faiss.as_retriever(search_type="similarity", search_kwargs={"k": 3}),
+    return_source_documents=True,
+    chain_type_kwargs={"prompt": PROMPT})
+  
+  answer = qa({"query": query})['result']
+  return answer
+        
 embeddings = HuggingFaceEmbeddings(
         model_name="sentence-transformers/all-MiniLM-L6-v2",
         model_kwargs={"device": "cpu"},
@@ -56,10 +67,10 @@ docs = text_splitter.split_documents(documents)
 
 vectorstore_faiss = FAISS.from_documents(docs, embeddings)
 
-#feed LLM
 # Main Streamlit app
-st.title("Chat PDF Using AWS Bedrock and Anthropic Claude")
-with st.sidebar:
+def main():
+    st.title("Chat PDF Using AWS Bedrock and Anthropic Claude")
+    with st.sidebar:
         st.title('💬 PDF Chat App')
         st.markdown('''
         ## About
@@ -67,38 +78,22 @@ with st.sidebar:
         ''')
         st.write('Made by Noa Cohen')
             
-prompt_template = """
+    prompt_template = """Human: Use the following pieces of context to provide a concise answer to the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
+                      <context>
+                      {context}
+                      </context
 
-Human: Use the following pieces of context to provide a concise answer to the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
-<context>
-{context}
-</context
+                     Question: {question}
 
-Question: {question}
+                     Assistant:"""
 
-Assistant:"""
-
-PROMPT = PromptTemplate(
-    template=prompt_template, input_variables=["context", "question"]
-)
+    PROMPT = PromptTemplate(
+            template=prompt_template, input_variables=["context", "question"])
 
 
 
-def create_RetrievalQA_chain(query):
-
-  qa = RetrievalQA.from_chain_type(
-    llm=llm,
-    chain_type="stuff",
-    retriever=vectorstore_faiss.as_retriever(search_type="similarity", search_kwargs={"k": 3}),
-    return_source_documents=True,
-    chain_type_kwargs={"prompt": PROMPT})
-  
-  answer = qa({"query": query})['result']
-  return answer
-
-
-user_input = st.text_area("Ask Your Question")
-button = st.button("Generate Answer")
-if user_input and button:
-    summary = create_RetrievalQA_chain(user_input)
-    st.write("Summary : ", summary)
+    user_input = st.text_area("Ask Your Question")
+    button = st.button("Generate Answer")
+    if user_input and button:
+            summary = create_RetrievalQA_chain(user_input)
+            st.write("Summary : ", summary)
