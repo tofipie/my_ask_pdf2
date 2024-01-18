7de6aa203e43576631946fd3ebff032860b5594f
tofipie <62646269+tofipie@users.noreply.github.com>
2024-01-18 17:37:49 +0200

    Update app6.py

--- app6.py
+++ app6.py
@@ -1,16 +1,13 @@
 import streamlit as st
-from langchain.document_loaders import PyMuPDFLoader
-#from loadllm import Loadllm
+from langchain.llms import HuggingFaceHub
 from streamlit_chat import message
 import tempfile
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chains import ConversationalRetrievalChain
-from langchain.llms import HuggingFaceHub
+from langchain.document_loaders import PyPDFLoader, PyPDFDirectoryLoader
 
-import streamlit as st
 
-from fileingestor import FileIngestor
 
 # Set the title for the Streamlit app
 st.title("Chat with PDF - 🦙 🔗")
@@ -18,8 +15,73 @@ st.title("Chat with PDF - 🦙 🔗")
 # Create a file uploader in the sidebar
 uploaded_file = st.sidebar.file_uploader("Upload File", type="pdf")
 
-if uploaded_file:
-    file_ingestor = FileIngestor(uploaded_file)
-    file_ingestor.handlefileandingest()
+DB_FAISS_PATH = "vectorstores/db_faiss"
+DATA_PATH = "pdfs/"
+
+loader = DirectoryLoader(DATA_PATH, glob="*.pdf", loader_cls=PyPDFLoader)
+documents = loader.load()
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=50)
+texts = text_splitter.split_documents(documents)
+
+embeddings = HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2",
+        model_kwargs={"device": "cpu"},
+    )
+
+db = FAISS.from_documents(texts, embeddings)
+db.save_local(DB_FAISS_PATH)
+    
+
+# Create embeddings using Sentence Transformers
+embeddings = HuggingFaceEmbeddings(model_name='sentence-transformers/all-MiniLM-L6-v2')
+
+# Create a FAISS vector store and save embeddings
+
+        # Load the language model
+llm = HuggingFaceHub(repo_id="google/flan-t5-xxl",
+                    model_kwargs={"temperature":0.5, "max_length":512},
+                             huggingfacehub_api_token='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')
+
+        # Create a conversational chain
+chain = ConversationalRetrievalChain.from_llm(llm=llm, retriever=db.as_retriever())
+
+        # Function for conversational chat
+def conversational_chat(query):
+    result = chain({"question": query, "chat_history": st.session_state['history']})
+    st.session_state['history'].append((query, result["answer"]))
+    return result["answer"]
+
+        # Initialize chat history
+if 'history' not in st.session_state:
+    st.session_state['history'] = []
+
+        # Initialize messages
+if 'generated' not in st.session_state:
+    st.session_state['generated'] = ["Hello ! Ask me(LLAMA2) about " + self.uploaded_file.name + " 🤗"]
+
+if 'past' not in st.session_state:
+    st.session_state['past'] = ["Hey ! 👋"]
+
+        # Create containers for chat history and user input
+response_container = st.container()
+container = st.container()
+
+        # User input form
+        with container:
+            with st.form(key='my_form', clear_on_submit=True):
+                user_input = st.text_input("Query:", placeholder="Talk to PDF data 🧮", key='input')
+                submit_button = st.form_submit_button(label='Send')
+
+            if submit_button and user_input:
+                output = conversational_chat(user_input)
+                st.session_state['past'].append(user_input)
+                st.session_state['generated'].append(output)
+
+        # Display chat history
+        if st.session_state['generated']:
+            with response_container:
+                for i in range(len(st.session_state['generated'])):
+                    message(st.session_state["past"][i], is_user=True, key=str(i) + '_user', avatar_style="big-smile")
+                    message(st.session_state["generated"][i], key=str(i), avatar_style="thumbs")
     
 
