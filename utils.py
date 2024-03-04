7a033ffa2a7d37cfcdb137b9fbd0e02ee5f1ef1c
tofipie <62646269+tofipie@users.noreply.github.com>
2024-03-04 14:24:41 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 def get_data_files():
     data_files = []
-    for dirname, _, filenames in os.walk("data"):
+    for dirname, _, filenames in os.walk("pdfs"):
         for filename in filenames:
             data_files.append(os.path.join(filename))
     return data_files
